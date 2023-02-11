4084d35a2e69d61d8c1eeaf2c9b1618ce4769ff7
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2023-02-10 19:29:19 -0700

    Add text and new route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hello World!</h1>
+<p>
+	This is a page built with SvelteKit. It's a single-page app, which means that the page loads
+	instantly and runs entirely in the browser.
+</p>
+
+<p>
+	There is another page at /gated. It's a protected route, which means that you need to be logged in
+	to see it.
+</p>
+
+<button id="login">Click to Login</button>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn more about SvelteKit.</p>
---
+++ src/routes/gated/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Gated Page</h1>
+
+<p>you cannot see this text unless you are logged in</p>
