c6314a01f2b2e3a24d2b280f5f7d949a05d3dc82
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2023-02-10 20:40:59 -0700

    Add gated route

--- src/routes/gated/+page.svelte
+++ src/routes/gated/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { isAuthenticated } from '$app/stores';
+
+    $: if (!isAuthenticated) goto('/')
+</script>
+
 <h1>Gated Page</h1>
 
-<p>you cannot see this text unless you are logged in</p>
+<p>You should not be able to see this page unless you are logged in!</p>
+
+<button on:click={goto('/')}>Return to homepage</button>
